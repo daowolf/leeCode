0f7dad96dd08157bdc61765bf244691261e231f0
songwenwu <wenwu07093@163.com>
2018-08-22 22:29:12 +0800

    @wenwu

--- array/src/com/wenwu/code/twoSum.java
+++ array/src/com/wenwu/code/twoSum.java
@@ -10,6 +10,13 @@ import java.util.HashMap;
  * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
  */
 public class twoSum {
+
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return manay
+     */
     public HashMap<Integer, Integer> twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> indexMap = new HashMap<>();
         int len = nums.length;
@@ -21,6 +28,26 @@ public class twoSum {
             }
         }
         return indexMap;
+    }
+
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return one
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int[] arr = new int[2];
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j <len ; j++) {
+                if(target == nums[i]+nums[j]){
+                    arr[0]=i;
+                    arr[1]=j;
+                }
+            }
+        }
+        return arr;
     }
 
     @Test
