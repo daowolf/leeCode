8af8278206a543bd57c4db4d358fa7231ac47d9c
songwenwu <wenwu07093@163.com>
2018-08-29 00:18:04 +0800

    @wenwu

---
+++ array/src/com/wenwu/code/searchRange.java
@@ -0,0 +1,29 @@
+package com.wenwu.code;
+
+import java.util.ArrayList;
+
+/**
+ *  给定一个按照升序排列的整数数组 nums，和一个目标值 target。
+ * 找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 如果数组中不存在目标值，返回 [-1, -1]。
+ */
+public class searchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int max = nums.length;
+        if(nums[max-1]<target || nums[0]>target)
+            return new int[] {-1,-1};
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < max ; i++) {
+            if(nums[i]<target&&nums[i+1]>target)
+                return new int[] {-1,-1};
+            else if(nums[i] == target){
+                integers.add(i);
+            }
+
+        }
+        return integers.toArray(int[] ints);
+    }
+}
